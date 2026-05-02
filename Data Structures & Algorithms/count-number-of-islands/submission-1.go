bf024cdfa06acceb3750ef1f88fbdb866b0fe81d
Dicky arya Kesuma <dickyaryakesuma@gmail.com>
2026-05-02 21:32:59 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.go
@@ -0,0 +1,52 @@
+func numIslands(grid [][]byte) int {
+
+    visitedElement := map[string]bool{}
+
+    result := 0
+
+    for i:=0;i<len(grid);i++{
+        for j:=0;j<len(grid[0]);j++{
+            if grid[i][j] == '0' || visitedElement[matrixKey(i,j)] {
+                continue
+            }
+    
+            result += 1
+            find(grid, visitedElement, i, j)
+
+        }
+    }
+
+    return result
+}
+
+func matrixKey(row, column int) string {
+    return fmt.Sprintf("%d:%d", row, column)
+}
+
+func find(grid [][]byte, visitedElement map[string]bool, pointerRow, pointerColumn int) {
+
+
+    if pointerRow < 0 || pointerRow >= len(grid) || pointerColumn < 0 || pointerColumn >= len(grid[0]) {
+        return
+    }
+    if grid[pointerRow][pointerColumn] == '0' {
+        return
+    }
+
+    key := matrixKey(pointerRow, pointerColumn)
+    if visitedElement[key] {
+        return
+    }
+    visitedElement[key] = true
+
+    find(grid, visitedElement, pointerRow, pointerColumn-1)
+    
+
+    find(grid, visitedElement, pointerRow-1, pointerColumn)
+    
+
+    find(grid, visitedElement, pointerRow+1, pointerColumn)
+    
+
+    find(grid, visitedElement, pointerRow, pointerColumn+1)
+} 
