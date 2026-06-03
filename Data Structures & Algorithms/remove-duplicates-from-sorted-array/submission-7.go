ee3f5f8d20b41651b6ea25171742979e3fe6f9ed
Dicky arya Kesuma <dickyaryakesuma@gmail.com>
2026-06-04 06:55:00 +0700

    Add: remove-duplicates-from-sorted-array - submission-7

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-7.go
@@ -0,0 +1,39 @@
+func removeDuplicates(nums []int) int {
+	pointer1 := 0
+	pointer2 := 1
+	duplicate := false
+
+	if len(nums) == 1 {
+		return 1
+	}
+
+	for {
+		if pointer1 == len(nums)-1 || pointer2 == len(nums)-1  {
+			if nums[pointer1] == nums[pointer2] {
+				nums = append(nums[:pointer1], nums[pointer2:]...)
+			} else {
+				nums = append(nums[:pointer1+1], nums[pointer2:]...)
+			}
+			return len(nums)
+		}
+
+		if nums[pointer1] == nums[pointer2] {
+			duplicate = true
+			if pointer2 < len(nums) {
+				pointer2 += 1
+			}
+		} else if duplicate == true {
+
+			nums = append(nums[:pointer1+1], nums[pointer2:]...)
+			duplicate = false
+
+			pointer2 = pointer1 + 1
+ 
+		} else {
+			pointer1 += 1
+			pointer2 += 1
+		}
+	}
+
+	return len(nums)
+}
