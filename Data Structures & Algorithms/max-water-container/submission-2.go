40df19e1ffa1349cc236d7ee31ce1b7ba7881c89
Dicky arya Kesuma <dickyaryakesuma@gmail.com>
2026-06-16 15:45:38 +0700

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.go
@@ -0,0 +1,37 @@
+func maxArea(heights []int) int {
+
+
+	p1 := 0
+	p2 := len(heights)-1
+
+	result := 0
+
+	for {
+		if p1 == p2 {
+			break
+		}
+
+		fmt.Println(p1, p2)
+
+		curr := (p2-p1) * min(heights[p1], heights[p2])
+		if curr > result {
+			result = curr
+		}
+
+		if heights[p1] == min(heights[p1], heights[p2]) {
+			p1+=1
+		} else {
+			p2-=1
+		}
+
+	}
+
+	return result
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
